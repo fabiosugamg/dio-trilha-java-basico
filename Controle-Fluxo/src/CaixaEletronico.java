c89357ea0df8e02b9e69b0f2335ec0f6f075fc43
suga <fabiosugamg@gmail.com>
2023-09-21 17:37:18 -0300

    inicial_contador

---
+++ Controle-Fluxo/src/CaixaEletronico.java
@@ -0,0 +1,10 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+         for (int x = 1; x <= 10; x++){
+           if(x % 2 == 0)
+           System.out.println(x);
+        }
+        
+    };
+    
+}
