bcb60fce26334ae27ea3483fe4ccf349a060da16
suga <fabiosugamg@gmail.com>
2023-09-21 18:20:08 -0300

    final

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -2,36 +2,34 @@ import java.util.Scanner;
 
 public class Contador {
 	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
+		 Scanner terminal = new Scanner(System.in);
 		
         int parametroUm, parametroDois;
 		
-        do {  
             System.out.println("Digite o primeiro parâmetro: ");
 		    parametroUm = terminal.nextInt();
 
 		    System.out.println("Digite o segundo parâmetro: ");
 		    parametroDois = terminal.nextInt();
-            
-            if (parametroUm > parametroDois){
-                System.out.println("O segundo parâmetro deve ser maior que o primeiro. Tente novamente. ");
-                }
-            }while (parametroUm > parametroDois);
-        
 
 		try {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
+		
 		}catch (ParametrosInvalidosException e) {
-			System.out.println(e.getMessage());
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro parâmetro. ");
             //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
+		if (parametroUm > parametroDois){
+			throw new ParametrosInvalidosException();
+		}
+
+
 		int contagem = parametroDois - parametroUm;
 		for(int i = 0; i < contagem; i++){
         System.out.println("Imprimindo o número " + (parametroUm + i));
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,4 +1,3 @@
 
 public class ParametrosInvalidosException extends Exception {
-    
-}
+    }
